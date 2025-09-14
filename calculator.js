cf31ddf77a2d0a9a1e71c78237142a36d94d3d22
Kaja Ali Mohiddin Sheik <sheik.kajaalimohiddin359@gmail.com>
2025-09-14 13:07:46 -0500

    second commit

--- calculator.js
+++ calculator.js
@@ -6,7 +6,11 @@ function subtract(a,b){
     return a-b;
 }
 
+function multiply(a,b){
+    return a*b;
+}
 module.exports={
     add,
-    subtract
+    subtract,
+    multiply
 };
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { add, subtract } = require("./calculator");
+const yargs = require("yargs");
+const { add, subtract,multiply } = require("./calculator");
+
+const options = yargs(process.argv);
 
 console.log("hello world");
-console.log(add(2+3));
-console.log(subtract(2-3));
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
